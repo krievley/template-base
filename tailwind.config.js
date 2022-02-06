d3f6bbd28fd122c594773051d368bc38aaf359d9
Kristin <kristin.collins916@gmail.com>
2022-02-06 12:25:00 -0500

    Add dark theme toggle.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,23 @@
 module.exports = {
-  content: [
-    "./*.{html,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-        colors: {
-            brand: '#5F6FA3',
-            light: '#F3F1F2',
-            lightAccent: '#8E9BB3',
-            dark: '#272829',
-            darkAccent: '#226DEC',
-            primary: '#5F6FA3',
-            info: '#272829',
-            success: '#529c69',
-            warning: '#cf8c31',
-            danger: '#f44336',
-        }
+    darkMode: 'class',
+    content: [
+        "./*.{html,js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                brand: '#5F6FA3',
+                light: '#F3F1F2',
+                lightAccent: '#8E9BB3',
+                dark: '#272829',
+                darkAccent: '#226DEC',
+                primary: '#5F6FA3',
+                info: '#272829',
+                success: '#529c69',
+                warning: '#cf8c31',
+                danger: '#f44336',
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
