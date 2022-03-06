4122a4d4f95de365b91358e7db79ea39b21790fb
Kristin <kristin.collins916@gmail.com>
2022-03-06 12:57:27 -0500

    Finish color and template changes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,21 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                brand: '#5F6FA3',
-                light: '#F3F1F2',
-                lightAccent: '#8E9BB3',
-                dark: '#272829',
-                darkAccent: '#226DEC',
+                brand: '#eec83e',
+                light: '#ecf0ee',
+                lightAccent: '#1c99ef',
+                dark: '#000000',
+                darkAccent: '#e52123',
                 primary: '#5F6FA3',
                 info: '#272829',
                 success: '#529c69',
                 warning: '#cf8c31',
                 danger: '#f44336',
             }
+        },
+        fontFamily: {
+            sans: ['Jedi', 'sans-serif'],
+            serif: ['Open Sans', 'serif']
         },
     },
     plugins: [],
